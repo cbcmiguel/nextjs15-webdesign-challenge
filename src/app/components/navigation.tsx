cc84e013f4b90d55d918d48b8c5a8e9dea1272a3
CbcMiguel28 <miguelcabungcag4@gmail.com>
2025-02-09 15:59:56 +0800

    Updated Style

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,13 +1,17 @@
 "use client"
 import Link from "next/link";
+import Image from "next/image";
 
 
 export const Navigation = () =>{
     return(
-        <nav>
-            <Link href="/" className= "/"> Home</Link>
-            <Link href="about" className="/about">About</Link>
-            <Link href="about" className="/contact">Contact</Link>
-        </nav>
+        <div className="flex flex-row justify-between my-[12px]">
+            <Image src="/aique_logo.svg" alt="Logo" className="p-4" width={64} height={64} priority/>
+             <nav className="flex gap-[12px] items-center">
+                <Link href="/" className= "bg-[#2D68F8] text-white px-6 py-2 rounded-lg"> Home</Link>
+                <Link href="about" className="px-6 py-2 rounded-lg">About</Link>
+                <Link href="about" className="px-6 py-2 rounded-lg">Contact</Link>
+            </nav>
+        </div>
     );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
+
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 import "./globals.css";
 import { Navigation } from "./components/navigation";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +21,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased max-w-[1480px] mx-auto px-[124px]`}
       >
         <header>
           <Navigation/>
